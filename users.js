e301ccef4de121595ae276cc4622b25b4fa7cafa
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2020-11-15 13:33:51 -0500

    added images and created user cards

---
+++ users.js
@@ -0,0 +1,109 @@
+
+const users = [
+        {
+            "id": 0,
+            "name": "Mohameth Seck",
+            "service": "Web Developer",
+            "location": "Dakar",
+            "lang": "English",
+            "img": "../"
+        },
+        {
+            "id": 1,
+            "name": "Aziz Seck",
+            "service": "Chef",
+            "location": "Kaolack",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 2,
+            "name": "Fatima Seck",
+            "service": "UI/UX Designer",
+            "location": "Saly",
+            "lang": "Arabic, French",
+            "img": "../"
+        },
+        {
+            "id": 3,
+            "name": "Seydou",
+            "service": "Carpenter",
+            "location": "Dakar",
+            "lang": "Wolof, French",
+            "img": "../"
+        },
+        {
+            "id": 4,
+            "name": "Khadidja",
+            "service": "Graphic Designer",
+            "location": "Saint Louis",
+            "lang": "English, Wolof",
+            "img": "../"
+        },
+        {
+            "id": 5,
+            "name": "Aissa",
+            "service": "Fashion Designer",
+            "location": "Dakar",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 6,
+            "name": "Ndambao Sene",
+            "service": "Chef",
+            "location": "Dakar",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 7,
+            "name": "Lala",
+            "service": "Chef",
+            "location": "Saint Louis",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 8,
+            "name": "Hakeem",
+            "service": "Hair Stylist",
+            "location": "Linguere",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 9,
+            "name": "Aziz Seck",
+            "service": "House Keeper",
+            "location": "Dakar",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 10,
+            "name": "Aziz Seck",
+            "service": "Musician",
+            "location": "Dakar",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 11,
+            "name": "Amina",
+            "service": "Videography",
+            "location": "Theis",
+            "lang": "English, French",
+            "img": "../"
+        },
+        {
+            "id": 12,
+            "name": "Semba",
+            "service": "Artist",
+            "location": "Dakar",
+            "lang": "English, French",
+            "img": "../"
+        }
+    ]
+
+    export default users;
