aabb6cce3d27411992dfcef124952e1dc69bd17d
Mohameth Seck <30297403+SeckMohameth@users.noreply.github.com>
2020-11-15 13:34:24 -0500

    old changes removed or altered

--- components/Freelancer.js
+++ components/Freelancer.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import styles from '../styles/Home.module.css'
 
+import data from '../users';
 
-function Freelancer(props){
-    return (
-        <div className="Card">
-            <div className="content">
-                <img />
+//maping the user object to the card component
+const users = data.map(item => 
+        <div className={styles.card}>
+            <div className="Card-details">
+                <h3>{item.name}</h3>
+                <h4>{item.service}</h4>
+                <p>{item.location}</p>
+                <p>{item.lang}</p>
             </div>
-            <Button color="primary">
+            <Button color="secondary">
                 View Profile
             </Button>
+        </div>
+    );
+
+
+function Freelancer(){
+    return (
+        <div>
+            {users}
         </div>
     )
 }
